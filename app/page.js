fa407f51464a48eb101a33a3d0c262b8e6137e53
Givik <givi.kuchukhidze@gmail.com>
2023-06-18 22:52:16 +0400

    changes

--- app/page.js
+++ app/page.js
@@ -1,95 +1,49 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
 
-export default function Home() {
+const schema = {
+  city: 'string',
+  lat: 'string',
+  lng: 'string',
+  country: 'string',
+  iso2: 'string',
+  admin_name: 'string',
+  capital: 'string',
+  population: 'string',
+  population_proper: 'string',
+};
+
+const App = () => {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div id="item">
+        <div className="poster">
+          <img width="120" src="https://image.tmdb.org/t/p/w500/z2iuBcwznen3kC9z4LeOzBSz1BB.jpg" />
+        </div>
+        <div className="about">
+          <div>
+            <strong>id: </strong>460071
+          </div>
+          <div>
+            <strong>title: </strong>Lizzie
+          </div>
+          <div className="overview">
+            <strong>overview: </strong> Massachusetts, 1892. An unmarried woman of 32 and a social
+            outcast, Lizzie lives a claustrophobic life under her father's cold and domineering
+            control. When Bridget Sullivan, a young maid, comes to work for the family, Lizzie finds
+            a sympathetic, kindred spirit, and a secret intimacy soon blossoms into a wicked plan.
+          </div>
+          <div>
+            <strong>genres: </strong>Crime, Drama, Thriller
+          </div>
+          <div>
+            <strong>release_date: </strong>1536883200
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
-}
+  );
+};
+
+export default App;
---
+++ components/search/index.js
@@ -0,0 +1,51 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { Index } from 'flexsearch';
+import { encode, rtl } from './dist/module/lang/latin/extra.js';
+import styles from './styles.module.css';
+
+let data = {};
+
+const Search = () => {
+  const [index, setIndex] = useState(new Index({ encode: encode }));
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      //  fetch data to search
+      const res = await fetch('/movies.json');
+      const arrayRes = await res.json();
+
+      console.log('%c fetching done ', 'color: orange');
+
+      arrayRes.forEach((item, curIndex) => {
+        data[curIndex + 1] = { id: parseInt(curIndex + 1), title: item.title };
+        //  When the component first loads, we need to iterate
+        // through data values and add each to the search index.
+        setIndex(index.add(parseInt(curIndex + 1), item.title));
+      });
+
+      console.log('%c iteration done ', 'color: grey');
+    };
+
+    fetchData().catch(console.error);
+  }, []);
+
+  useEffect(() => {
+    setResults(index.search(query));
+  }, [query]);
+
+  return (
+    <div>
+      <input value={query} onChange={(e) => setQuery(e.target.value)} />
+      <ul>
+        {results.map((result) => {
+          if (data[result]) return <li key={result}>{data[result].title}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Search;
