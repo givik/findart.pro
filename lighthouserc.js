4f8a4de32673f4bfd63418aa863cfc24670afe3d
Givik <givi.kuchukhidze@gmail.com>
2023-07-07 18:09:18 +0400

    add min scores to lighthouse config

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,6 +5,12 @@ module.exports = {
     },
     assert: {
       preset: 'lighthouse:recommended',
+      assertions: {
+        'categories:performance': ['error', { minScore: 0.95 }],
+        'categories:accessibility': ['error', { minScore: 0.95 }],
+        'categories:best-practices': ['error', { minScore: 0.95 }],
+        'categories:seo': ['error', { minScore: 0.95 }],
+      },
     },
   },
 };
