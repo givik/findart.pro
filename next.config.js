a4cced833e5d4a5b2506416a34d623fc0e438b97
Givi <givi.kuchukhidze@gmail.com>
2023-12-09 07:38:34 +0400

    changes

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  rewrites: async () => [
-    {
-      source: '/landing',
-      destination: '/landing/index.html',
-    },
-  ],
 };
 
 module.exports = nextConfig;
