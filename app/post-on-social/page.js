126dea4d1258fffa6a8e39f356597b7bfba52293
Givi <givi.kuchukhidze@gmail.com>
2023-12-11 22:09:53 +0400

    need working

--- app/post-on-social/page.js
+++ app/post-on-social/page.js
@@ -1,5 +1,10 @@
 'use client';
 import { useEffect } from 'react';
+import { series } from 'async';
+const { exec } = require('child_process');
+
+//! need work
+series([() => exec('npm run dev'), () => exec('npm run test')]);
 
 const App = () => {
   useEffect(() => {
