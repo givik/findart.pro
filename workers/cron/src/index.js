d73d6684da08edc8745a627c8c9a080253d0667d
Givi <givi.kuchukhidze@gmail.com>
2023-12-11 17:46:36 +0400

    changes

---
+++ workers/cron/src/index.js
@@ -0,0 +1,35 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npx wrangler dev src/index.js` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npx wrangler publish src/index.js --name my-worker` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export default {
+  async fetch(request, env, ctx) {
+    return new Response('Hello World!');
+  },
+};
+
+addEventListener('fetch', (event) => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+  return new Response('Hello worker!', {
+    headers: { 'content-type': 'text/plain' },
+  });
+}
+
+addEventListener('scheduled', (event) => {
+  event.waitUntil(doScheduledThing(event.request));
+});
+
+async function doScheduledThing(request) {
+  // Go update an API
+  // Get something like weather data
+  return new Response('OK');
+}
