08aabe4fe7e4e31c409bc0bafb4ba703ecd9e378
Givi <givi.kuchukhidze@gmail.com>
2023-12-12 14:30:13 +0400

    changes

--- app/post-on-social/page.js
+++ app/post-on-social/page.js
@@ -3,12 +3,10 @@ import { useEffect } from 'react';
 import { series } from 'async';
 const { exec } = require('child_process');
 
-//! need work
-series([() => exec('npm run dev'), () => exec('npm run test')]);
-
 const App = () => {
   useEffect(() => {
-    // do something
+    //! need work
+    series([() => exec('npm run dev'), () => exec('npm run test')]);
   }, []);
 
   return <div>ok</div>;
