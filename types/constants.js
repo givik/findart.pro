ca2fc3df27a2c2fa619ce377d50524b67dca538a
Givi <givi.kuchukhidze@gmail.com>
2023-12-13 18:16:33 +0400

    changes

--- types/constants.js
+++ types/constants.js
@@ -1,13 +1,9 @@
-import { z } from "zod";
-export const COMP_NAME = "MyComp";
+import { z } from 'zod';
+export const COMP_NAME = 'MyComp';
 
 export const CompositionProps = z.object({
-  title: z.string(),
+  title,
 });
-
-export const defaultMyCompProps: z.infer<typeof CompositionProps> = {
-  title: "Next.js and Remotion",
-};
 
 export const DURATION_IN_FRAMES = 200;
 export const VIDEO_WIDTH = 1280;
