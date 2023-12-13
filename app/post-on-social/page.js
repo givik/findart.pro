712c7c40b46c4425d700458af112a129fa0f832a
Givi <givi.kuchukhidze@gmail.com>
2023-12-13 18:42:33 +0400

    god

--- app/post-on-social/page.js
+++ app/post-on-social/page.js
@@ -3,12 +3,12 @@ const exec = require('child_process').exec;
 
 const App = () => {
   series([() => exec('npm run render MyComp')]);
-  series([
-    () =>
-      exec(
-        'curl -X GET "https://graph.facebook.com/oauth/access_token?client_id={your-app-id}&client_secret={your-app-secret}&grant_type=client_credentials"'
-      ),
-  ]);
+  // series([
+  //   () =>
+  //     exec(
+  //       'curl -X GET "https://graph.facebook.com/oauth/access_token?client_id={your-app-id}&client_secret={your-app-secret}&grant_type=client_credentials"'
+  //     ),
+  // ]);
   return <div>ok</div>;
 };
 
