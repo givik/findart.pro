8ecc7b16527ef732e0e4b4a4ee44aaf01c45c1bc
Givi <givi.kuchukhidze@gmail.com>
2023-12-14 17:05:19 +0400

    changes

--- app/page.js
+++ app/page.js
@@ -19,7 +19,7 @@ const App = () => {
           onClick={onLogoClick}
         />
       </div>
-      <div>ქრისტეა ღმერთი!!!</div>
+      <div>სიყვარულია</div>
       <Search onLogoClick />
     </main>
   );
