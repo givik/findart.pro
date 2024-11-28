49d4f37d7b7051c850b4e9643eaca490025769f0
Givi <givi.kuchukhidze@gmail.com>
2024-11-29 02:55:39 +0400

    changes

--- app/cat/Creatures/page.js
+++ app/cat/Creatures/page.js
@@ -36,7 +36,7 @@ const Tab1Page = () => {
 
   return (
     <div>
-      {loading ? <p>Loading...</p> : null}
+      {loading ? <p>Loading. Please Wait...</p> : null}
       <div
         style={{
           overflow: 'auto', // Enable scrolling
