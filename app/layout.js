6344d721c79d52d83e707918b7090f3a113b2c0d
Givi <givi.kuchukhidze@gmail.com>
2024-12-01 08:57:10 +0400

    google analytics

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google';
 import './styles.css';
 import Header from '../components/header';
 import Footer from '../components/footer';
+// import { GA_TRACKING_ID } from '../lib/ga';
 
 export const metadata = {
   title: 'FindArt.pro',
@@ -16,6 +17,19 @@ export default function RootLayout({ children: Content }) {
       <head>
         <meta name="google-adsense-account" content="ca-pub-9123253083798474"></meta>
         <link rel="icon" type="image/x-icon" href="fav.png"></link>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=G-5J0LS2Y216`} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-5J0LS2Y216');
+              `,
+          }}
+        />
       </head>
       <body className={inter.className}>
         <div id="site">
